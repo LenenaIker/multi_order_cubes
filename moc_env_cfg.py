bd06d733c360609a69dfba2674e871330fc7eab9
LenenaIker <iker12lenena@gmail.com>
2026-08-28 23:30:48 +0200

    Best reach

--- moc_env_cfg.py
+++ moc_env_cfg.py
@@ -99,11 +99,6 @@ class RewardsCfg:
         params=dict(k_z=0.06, p=1.0, gate_dxy=0.18, gate_band=0.05),
     )
 
-    approach_forward = RewTerm(
-        func=mdp.reward_gripper_approach_forward,
-        weight=6.0,
-        params=dict(k_dir=0.35, p=1.0),
-    )
 
     next_signal = RewTerm(
         func=mdp.reward_next_signal,
